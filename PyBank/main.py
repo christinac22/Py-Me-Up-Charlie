8254b447757a43f77e9212cd1f11507f84d8a174
christinac22 <christinachau96@gmail.com>
2022-03-06 20:27:12 -0800

    images for readme

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,12 +2,13 @@ import os
 import csv
 
 #set path to file
-budget_csv=os.path.join("Resources", "budget_data.csv")
+budget_csv=os.path.join("Resources/budget_data.csv")
 
 total_months = 0
 net_total_amount = 0
+Data = []
 average_change_list = []
-max_profit = []
+max_profit_list = []
 min_profit = []
 
 #open csv - see 1:59:38 lesson 3.2
@@ -17,20 +18,35 @@ with open(budget_csv) as csvfile:
     #reads header row
     csv_header = next(csv_reader)
     
-#looping through and counting number of months 
+#looping through and add number of months to data list
     for row in csv_reader:
-
         total_months = total_months + 1
+        Data.append(row[1])
+
 
 #net total 
         net_total_amount = net_total_amount + int(row[1])
+        # print(net_total_amount)
 
+#months total
+    i = 1 
+    for i in range(1, len(Data)):
+        # print(i)
 
-#average change
-        #average_change = round(int(net_total_amount) / int(total_months),2)
+# #average change
+        average_change_list.append(int(Data[i])-int(Data[i-1]))
+        print(average_change_list)
+
+    avg = round(sum(average_change_list)/len(average_change_list),2)
+    print(avg)
         
 #greatest increase in profits
     # for row in csv_reader
+    # x = 1 
+    # for x in range(1, len(Data)):
+    #     max_profit_list.append(int(Data[i])-int(Data[i-1]))
+    #     print(max_profit_list)
+
 
 #greatest decrease in profits
    
@@ -39,6 +55,6 @@ with open(budget_csv) as csvfile:
     print("------------------------")
     print("Total Months: " + str(total_months))
     print("Total: " + "$" +str(net_total_amount))
-    # print("Average Change: " + "$" + str(average_change_list))
+    print("Average Change: " + "$" + str(avg))
     # print("Greatest Increase in Profits: " + str(max_profit))
     # print("Greatest Decrease in Profits: " + str(min_profit))
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,16 +3,20 @@ import csv
 
 election_csv=os.path.join("Resources", "election_data.csv")
 
+#variables
 total_votes = 0
-
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
 
 #open csv - see 1:59:38 lesson 3.2
 with open(election_csv) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
-    print(csv_reader)
+    # print(csv_reader)
     #reads header row
     csv_header = next(csv_reader)
-    #print(csv_header)
+    # print(csv_header)
    
 #looping through csv and counting number of votes by row
     for row in csv_reader:
@@ -20,15 +24,31 @@ with open(election_csv) as csvfile:
         total_votes = total_votes + 1
 
 #votes for each candidate
-# for candidate in candidates:
-
-          
+        if row[2] == "Khan":
+            khan_votes = khan_votes + 1
+        elif row[2] == "Correy":
+            correy_votes = correy_votes + 1
+        elif row[2] =="Li":
+            li_votes = li_votes + 1
+        elif row[2] == "O'Tooley":
+            otooley_votes = otooley_votes + 1
+        
 #percent of votes each candidate won
+    khan_percent = round((khan_votes/total_votes)*100,2)
+    correy_percent = round((correy_votes/total_votes)*100,2)
+    li_percent = round((li_votes/total_votes)*100,2)
+    otooley_percent = round((otooley_votes/total_votes)*100,2)
 
 #determine winner
 
 
     #Analysis
-    print("Election Results")
-    print("------------------------")
-    print("Total Votes: " + str(total_votes))
+    print(f"Election Results")
+    print(f"----------------------------------")
+    print(f"Total Votes: {total_votes}")
+    print(f"----------------------------------")
+    print(f"Khan: {khan_percent}% ({khan_votes})")
+    print(f"Correy: {correy_percent}% ({correy_votes})")
+    print(f"Li: {li_percent}% ({li_votes})")
+    print(f"O'Tooley: {otooley_percent}% ({otooley_votes})")
+
