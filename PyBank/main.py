90b5d87059b67b7b56599a2baf45b9eb7024f408
christinac22 <christinachau96@gmail.com>
2022-04-13 23:49:55 -0700

    additonal updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,60 +1,69 @@
+# Modules
 import os
 import csv
 
-#set path to file
+# Set path for file
 budget_csv=os.path.join("Resources/budget_data.csv")
 
 total_months = 0
 net_total_amount = 0
 Data = []
-average_change_list = []
-max_profit_list = []
-min_profit = []
+average_change = []
 
-#open csv - see 1:59:38 lesson 3.2
+# Open csv - see 1:59:38 lesson 3.2
 with open(budget_csv) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
     print(csv_reader)
-    #reads header row
+    # Reads header row first
     csv_header = next(csv_reader)
     
-#looping through and add number of months to data list
+# Loop through and add number of months to data list
     for row in csv_reader:
         total_months = total_months + 1
         Data.append(row[1])
 
-
-#net total 
+# Net total 
         net_total_amount = net_total_amount + int(row[1])
         # print(net_total_amount)
 
-#months total
+# Months total
     i = 1 
     for i in range(1, len(Data)):
         # print(i)
 
-# #average change
-        average_change_list.append(int(Data[i])-int(Data[i-1]))
-        print(average_change_list)
+# Average change - current row minue previous row
+        average_change.append(int(Data[i])-int(Data[i-1]))
+        # print(average_change_list)
 
-    avg = round(sum(average_change_list)/len(average_change_list),2)
-    print(avg)
+    avg = round(sum(average_change)/len(average_change),2)
+    # print(avg)
         
-#greatest increase in profits
-    # for row in csv_reader
-    # x = 1 
-    # for x in range(1, len(Data)):
-    #     max_profit_list.append(int(Data[i])-int(Data[i-1]))
-    #     print(max_profit_list)
+# Greatest increase in profits
+    max_profit = max(average_change)
+    # print(max_profit)
 
-
-#greatest decrease in profits
+# Greatest decrease in profits
+    min_profit = min(average_change)
+    # print(max_profit)
    
-
+# Analysis
     print("Financial Analysis")
     print("------------------------")
     print("Total Months: " + str(total_months))
     print("Total: " + "$" +str(net_total_amount))
     print("Average Change: " + "$" + str(avg))
-    # print("Greatest Increase in Profits: " + str(max_profit))
-    # print("Greatest Decrease in Profits: " + str(min_profit))
+    print("Greatest Increase in Profits: " + "$" + str(max_profit))
+    print("Greatest Decrease in Profits: " + "$" + str(min_profit))
+
+# Export to csv
+# Reference: https://www.pythontutorial.net/python-basics/python-write-text-file/
+# Reference: https://stackoverflow.com/questions/11497376/how-do-i-specify-new-lines-on-python-when-writing-on-files 
+analysis = open("budget.txt","w")
+analysis.write("""
+Financial Analysis
+----------------------------------
+Total Months: 86
+Total: $38382578
+Average Change: $-2315.12
+Greatest Increase in Profits: $1926159
+Greatest Decrease in Profits: $-2196167 """)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,29 +1,31 @@
+# Modules
 import os
 import csv
 
+# Set path for file
 election_csv=os.path.join("Resources", "election_data.csv")
 
-#variables
+# Variables
 total_votes = 0
 khan_votes = 0
 correy_votes = 0
 li_votes = 0
 otooley_votes = 0
 
-#open csv - see 1:59:38 lesson 3.2
+# Open csv - see 1:59:38 lesson 3.2
 with open(election_csv) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
     # print(csv_reader)
-    #reads header row
+    # Reads header row
     csv_header = next(csv_reader)
     # print(csv_header)
    
-#looping through csv and counting number of votes by row
+# Loop through csv and count number of votes 
     for row in csv_reader:
         #total votes
         total_votes = total_votes + 1
 
-#votes for each candidate
+# Votes for each candidate
         if row[2] == "Khan":
             khan_votes = khan_votes + 1
         elif row[2] == "Correy":
@@ -33,16 +35,23 @@ with open(election_csv) as csvfile:
         elif row[2] == "O'Tooley":
             otooley_votes = otooley_votes + 1
         
-#percent of votes each candidate won
+# Percent of votes each candidate won
     khan_percent = round((khan_votes/total_votes)*100,2)
     correy_percent = round((correy_votes/total_votes)*100,2)
     li_percent = round((li_votes/total_votes)*100,2)
     otooley_percent = round((otooley_votes/total_votes)*100,2)
 
-#determine winner
+# Determine winner
+    if khan_votes > correy_votes and li_votes and otooley_votes:
+        winner = "Khan"
+    elif correy_votes > khan_votes and li_votes and otooley_votes:
+        winner = "Correy"
+    elif li_votes > khan_votes and correy_votes and otooley_votes:
+        winner = "Li"
+    else:
+        winner = "O'Tooley"
 
-
-    #Analysis
+# Analysis
     print(f"Election Results")
     print(f"----------------------------------")
     print(f"Total Votes: {total_votes}")
@@ -51,4 +60,24 @@ with open(election_csv) as csvfile:
     print(f"Correy: {correy_percent}% ({correy_votes})")
     print(f"Li: {li_percent}% ({li_votes})")
     print(f"O'Tooley: {otooley_percent}% ({otooley_votes})")
+    print(f"----------------------------------")
+    print(f"Winner: {winner}")
+    print(f"----------------------------------")
 
+
+# Export to csv
+# Reference: https://www.pythontutorial.net/python-basics/python-write-text-file/
+# Reference: https://stackoverflow.com/questions/11497376/how-do-i-specify-new-lines-on-python-when-writing-on-files 
+analysis = open("election.txt","w")
+analysis.write("""
+Election Results
+----------------------------------
+Total Votes: 3521001
+----------------------------------
+Khan: 63.0% (2218231)
+Correy: 20.0% (704200)
+Li: 14.0% (492940)
+O'Tooley: 3.0% (105630)
+----------------------------------
+Winner: Khan
+----------------------------------""")
